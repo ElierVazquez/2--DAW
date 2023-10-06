8a25e10dd4d46e18dae1075faf729cfb65968474
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2023-10-06 19:09:37 +0200

    Añadida nueva función de calculadora

--- DWS/php/Ud2/Ejercicios/calculadora.php
+++ DWS/php/Ud2/Ejercicios/calculadora.php
@@ -68,11 +68,7 @@
                 return false;
         
             }
-
-            $str=strtolower(str_replace([" ", ",", "."], "", $str));
-        
-         
-        
+            
             for ($i=0;$i<strlen($str);$i++) {
         
                 if ($str[$i]!=$str[strlen($str)-$i-1]) {
@@ -84,5 +80,53 @@
             }
         
             return true;
+        }
+
+        function sumaMatrices ($m1, $m2)
+        {
+            $m1Col = count($m1);
+            $m1Fil = count($m1[0]);
+
+            $m2Col = count($m2);
+            $m2Fil = count($m2[0]);
+
+            if ($m1Col != $m2Col || $m1Fil != $m2Fil)
+            {
+                $bool = false;
+            }
+            else
+            {
+                $bool = true;
+            }
+
+            $m3 = array();
+
+            if ($bool)
+            {
+                for ($x = 0; $x < $m1Col; $x++)
+                {
+                    for ($y = 0; $y < $m1Fil; $y++)
+                    {
+                        $m3[$x][$y] = $m1[$x][$y] + $m2[$x][$y];
+                    }
+                }
+            }
+            else
+            {
+                echo "Algo va mal mameluco.";
+            }
+
+            return $m3;
+        }
+
+        function pintarMatriz ($matrix)
+        {
+            $columnas = count($matrix);
+            $filas = count($matrix[0]);
+
+            for ($x = 0; $x < $columnas; $x++)
+            {
+                echo implode(", ", $matrix[$x])."<br/>";
+            }
         }
     }
--- DWS/php/Ud2/Ejercicios/calculadora_usuario.php
+++ DWS/php/Ud2/Ejercicios/calculadora_usuario.php
@@ -44,8 +44,29 @@
         return $x;
     }
 
+    function test6 ()
+    {
+        $m1 = array(
+            array(1, 4, 5),
+            array(8, 4, 7),
+            array(2, 6, 3)
+        );
+
+        $m2 = array(
+            array(5, 6, 3),
+            array(7, 4, 8),
+            array(1, 5, 9)
+        );
+
+        $calculadora = new calculadora();
+        $x = $calculadora->sumaMatrices($m1, $m2);
+
+        $y = $calculadora->pintarMatriz($x);
+    }
+
     echo "Factorial de 10: ".test1()."<br/>";
     echo "Coeficiente binomial de 5 y 3: ".test2()."<br/>";
     echo "Decimal del binario 101101000101: ".test3()."<br />";
     echo "Suma de números pares: ".test4()."<br />";
-    echo "La palabra apapa es palindromo: ".test5();
+    echo "La palabra apapa es palindromo: ".test5()."<br />";
+    echo test6();
