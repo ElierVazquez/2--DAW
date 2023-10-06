fbfa4c74869c0b4607c7218705e8ed25b39f26aa
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2023-10-06 17:44:38 +0200

    maldito palindromo

--- DWS/php/Ud2/Ejercicios/calculadora.php
+++ DWS/php/Ud2/Ejercicios/calculadora.php
@@ -38,6 +38,51 @@
 
         function coeficienteBinomial($n, $k)
         {
-            return $this->factorial($n)/$this->factorial($k)*$this->factorial($n - $k);
+            return $this->factorial($n)/($this->factorial($k)*$this->factorial($n - $k));
+        }
+
+        function convierteBinarioDecimal ($num)
+        {
+            return bindec($num);
+        }
+
+        function sumaNumerosPares ($array)
+        {
+            $suma = 0;
+
+            foreach ($array as $x)
+            {
+                if ($array[$x-1] % 2 == 0)
+                {
+                    $suma = $suma + $array[$x-1];
+                }
+            }
+
+            return $suma;
+        }
+
+        function esPalindromo ($str)
+        {
+            if (strlen($str)<2) {
+
+                return false;
+        
+            }
+
+            $str=strtolower(str_replace([" ", ",", "."], "", $str));
+        
+         
+        
+            for ($i=0;$i<strlen($str);$i++) {
+        
+                if ($str[$i]!=$str[strlen($str)-$i-1]) {
+        
+                    return false;
+        
+                }
+        
+            }
+        
+            return true;
         }
     }
--- DWS/php/Ud2/Ejercicios/calculadora_usuario.php
+++ DWS/php/Ud2/Ejercicios/calculadora_usuario.php
@@ -7,4 +7,45 @@
     function test1 ()
     {
         $calculadora = new calculadora();
+        $x = $calculadora->factorial(10);
+
+        return $x;
     }
+
+    function test2 ()
+    {
+        $calculadora = new calculadora();
+        $x = $calculadora->coeficienteBinomial(5, 3);
+
+        return $x;
+    }
+
+    function test3 ()
+    {
+        $calculadora = new calculadora();
+        $x = $calculadora->convierteBinarioDecimal(101101000101);
+
+        return $x;
+    }
+
+    function test4 ()
+    {
+        $calculadora = new calculadora();
+        $x = $calculadora->sumaNumerosPares(array(1, 2, 3, 4, 5, 6));
+
+        return $x;
+    }
+
+    function test5 ()
+    {
+        $calculadora = new calculadora();
+        $x = $calculadora->esPalindromo("apapa");
+
+        return $x;
+    }
+
+    echo "Factorial de 10: ".test1()."<br/>";
+    echo "Coeficiente binomial de 5 y 3: ".test2()."<br/>";
+    echo "Decimal del binario 101101000101: ".test3()."<br />";
+    echo "Suma de números pares: ".test4()."<br />";
+    echo "La palabra apapa es palindromo: ".test5();
