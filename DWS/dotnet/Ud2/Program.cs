55bcdc7eb6f4940b00a3845e8ee812e7c4bf22db
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2023-10-11 19:07:08 +0200

    Me cago en mi puta madre

---
+++ DWS/dotnet/Ud2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static int Act1 (int[,] array)
+        {
+            int columnas = array.GetLength(0)-1;
+            int filas = array.GetLength(1)-1;
+
+            bool suma = true;
+            int totalFila = 0;
+            int totalFilaAux = 0;
+
+            for (int fila1 = 0; fila1 <= filas; fila1++)
+            {
+                totalFila = totalFila + array[0,fila1];
+            }
+            
+            for (int i = 0; i <= columnas; i++)
+            {
+                for (int j = 0; j <= filas; j++)
+                {
+                    totalFilaAux = totalFilaAux + array[columnas,filas];
+                }
+
+                if (totalFila != totalFilaAux)
+                {
+                    suma = false;
+                    return totalFilaAux;
+                }
+
+                totalFilaAux = 0;
+            }
+
+            return totalFilaAux;
+        }
+
+        static void Main (string[] args)
+        {
+            /*
+            //Estructuras de repetición
+            Console.WriteLine("Estructuras de repetición\n");
+            for (int j = 0; j <= 7; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.WriteLine("\n");
+            int n = 10;
+            while (n>0)
+            {
+                n--;
+                Console.WriteLine(n);
+            }
+
+            // Arrays
+            Console.WriteLine("\nArrays\n");
+            int[] numbers = new int[2]{5, 25};
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            char[,] board = new char[3,3] {{'-','-','-'},
+                                           {'-','-','-'},
+                                           {'-','-','-'}};
+
+            /*
+            en board.GetLength(0) hace referencia a la primera dimensión
+            y en board.GetLength(1) hace referencia a la segunda dimensión,
+            por lo que no se referencia a las posiciones.
+            */ /*
+            int columnas = board.GetLength(0);
+            int filas = board.GetLength(1);
+
+            for (int columna = 0; columna < columnas; columna++)
+            {
+                for (int fila = 0; fila < filas; fila++)
+                {
+                    Console.Write(board[columna,fila] + "   ");
+                }
+                Console.WriteLine();
+            }
+
+            // Try catch
+            Console.WriteLine("\nTry catch\n");
+            try
+            {
+                int x = 10;
+                int y = x / 0;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            } */
+
+            int[,] array = new int[2,2] {{1, 2},
+                                        {2, 1}};
+
+            Console.WriteLine(Act1(array));
+        }
+    }
+}
