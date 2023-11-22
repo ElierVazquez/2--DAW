a5faf5f2394759a7b9c2108b024add72a31cb672
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2023-11-22 19:06:51 +0100

    Empezando proyecto

---
+++ DWS/UD3/Practica_2/src/vistas/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Chess</title>
+    <link rel="stylesheet" href="../../css/chess_game_styles.css">
+</head>
+<body>
+    <header>
+        <h1>Chess</h1>
+        <nav id="buttons">
+            <button>New Game</button>
+            <button>Games</button>
+        </nav>
+    </header>
+
+    <footer></footer>
+</body>
+</html>
