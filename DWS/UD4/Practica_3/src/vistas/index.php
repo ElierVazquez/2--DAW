ddcb939bc075400641bacd79b430eba948abfe67
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2024-01-10 19:35:14 +0100

    Empezando el inicio de sesión

--- DWS/UD4/Practica_3/src/vistas/index.php
+++ DWS/UD4/Practica_3/src/vistas/index.php
@@ -9,10 +9,23 @@
 <body>
     <header>
         <img src="../../img/gear.png" alt="Config" onclick="rotate()" id="config" class="">
+        <img src="../../img/user.png" alt="User management" id="users">
         <h1>Chess</h1>
         <nav id="buttons">
             <a href="new_gameView.php"><button>New Game</button></a>
             <a href="gameListView.php"><button>Game list</button></a>
+        </nav>
+        <nav id="user_management">
+            <nav>
+                <img src="../../img/login.png" alt="Login" id="login">
+            </nav>
+            <nav>
+                <img src="../../img/register.png" alt="Register" id="register">
+            </nav>
+            <hr>
+            <nav>
+                <img src="../../img/logout.png" alt="Logout" id="logout">
+            </nav>
         </nav>
     </header>
 
