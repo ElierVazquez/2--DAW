113ed3a0d8bd3e3245a05df43da75a5852d007fb
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2024-01-18 19:18:27 +0100

    Ejercicio

---
+++ DWC/Ejercicios/Botones con acciones/js/acciones.js
@@ -0,0 +1,53 @@
+function button1() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse down";
+}
+
+function button2() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse enter";
+}
+
+function button3() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse leave";
+}
+
+function button4() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse move";
+}
+
+function button5() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse out";
+}
+
+function button6() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse over";
+}
+
+function button7() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse up";
+}
+
+function button8() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse click";
+}
+
+function button9() {
+    let res = document.getElementById("res");
+
+    res.innerHTML = "Mouse doble click";
+}
