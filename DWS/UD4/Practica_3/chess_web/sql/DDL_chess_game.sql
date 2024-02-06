d4c45f0e04d9bec09d85e92b10c1f3e8205fe259
Elier Vazquez del Valle <eliervazquezdelvalle2002@gmail.com>
2024-02-06 14:06:10 +0100

    Corrección sql

--- DWS/UD4/Practica_3/chess_web/sql/DDL_chess_game.sql
+++ DWS/UD4/Practica_3/chess_web/sql/DDL_chess_game.sql
@@ -27,17 +27,18 @@ state varchar(20) not null default("En curso"),
 CREATE TABLE T_Board_Status(
 ID int auto_increment, 
 IDGame int,
-board varchar(250), /* Modificar si es necesario */
+board varchar(500), /* Modificar si es necesario */
+turn int,
 primary key(ID,IDGame),
 FOREIGN KEY (IDGame) REFERENCES T_Matches(ID)
 );
 
-INSERT INTO T_Players(name, email, password) 
+/* INSERT INTO T_Players(name, email, password, premium) 
     values
-("Elier", "eliervazquezdelvalle2002@gmail.com", "12345678"),
-("lokuelo23", "elloko@gmail.com", "MeGustaElAjedrez");
+("Elier", "eliervazquezdelvalle2002@gmail.com", "12345678", true),
+("lokuelo23", "elloko@gmail.com", "MeGustaElAjedrez", false); */ -- -> Hay que hashear las contraseñas, por el momento hay que crear a los usuarios por la página de registro.
 
-insert into T_Matches(title, startDate, state, white, black) 
+/* insert into T_Matches(title, startDate, state, white, black) 
 	values 
 ("Gámbito de dama", NOW(), "En curso", 1, 2);
 
@@ -62,3 +63,4 @@ insert into T_Board_Status(IDGame, turn, board) values (2, 0,
 
 insert into T_Board_Status(IDGame, turn, board) values (2, 1,
 "ROBL,KNBL,BIBL,QUBL,KIBL,BIBL,KNBL,ROBL_PABL,PABL,PABL,PABL,PABL,PABL,PABL,PABL_0,0,0,0,0,0,0,0_0,0,0,0,0,0,0,0_0,0,0,0,0,0,0,0_0,0,0,0,PAWH,0,0,0_PAWH,PAWH,PAWH,PAWH,0,PAWH,PAWH,PAWH_ROWH,KNWH,BIWH,QUWH,KIWH,BIWH,KNWH,ROWH");
+*/
