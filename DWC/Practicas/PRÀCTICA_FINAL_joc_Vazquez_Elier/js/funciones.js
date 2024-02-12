17d2a49f963ce4dbc892cf4dc0fd641ad75d995c
ElierVazquez <eliervazquezdelvalle2002@gmail.com>
2024-02-12 20:33:56 +0100

    Por favor dios, ayúdame

---
+++ DWC/Practicas/PRÀCTICA_FINAL_joc_Vazquez_Elier/js/funciones.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    let selected1 = null;
+    let selected2 = null;
+    $(".carta").click(function() {
+        let clickedCard = $(this).data("card-id");
+        let card1;
+        let card2;
+
+        if (selected1 == null) {
+            selected1 = clickedCard;
+            console.log("1 " + selected1);
+
+            card1 = document.getElementById(selected1);
+            card1.setAttribute("class", "carta selected");
+        } else if (selected1 != null) {
+            selected2 = clickedCard;
+            console.log("2 " + selected2);
+            if (ovejas(selected1, selected2)) {
+                console.log("opa gamnam style");
+            }
+
+            card1 = document.getElementById(selected1);
+            card1.setAttribute("class", "carta");
+
+            intervalo = setInterval(half_trisicsti, 10000, selected1, selected2);
+
+            clearInterval(intervalo);
+
+            card1 = document.getElementById(selected1);
+            card1.setAttribute("class", "carta");
+            card2 = document.getElementById(selected2);
+            card2.setAttribute("class", "carta");
+
+            selected1 = null;
+        }
+
+        
+    });
+});
+
+function half_trisicsti(selected1, selected2) {
+    let card1 = document.getElementById(selected1);
+    let card2 = document.getElementById(selected2);
+
+    card1.setAttribute("class", "carta rotate_phase1");
+    card2.setAttribute("class", "carta rotate_phase1");
+}
+
+function ovejas(blancas, negras) {
+    if (blancas == negras) {
+        return true;
+    }
+
+    return false;
+}
---
+++ DWC/Practicas/PRÀCTICA_FINAL_joc_Vazquez_Elier/memory_game.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Jogo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="js/funciones.js"></script>
+</head>
+<body>
+    <div>
+        <?php
+            echo $_POST['nickname'];
+        ?>
+    </div>
+    <div id="juego">
+        <table>
+            <tr>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="0-0" id="0-0" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="1-0" id="1-0" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="2-0" id="2-0" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="3-0" id="3-0" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="4-0" id="4-0" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="5-0" id="5-0" class="carta"></td>
+            </tr>
+            <tr>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="0-1" id="0-1" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="1-1" id="1-1" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="2-1" id="2-1" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="3-1" id="3-1" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="4-1" id="4-1" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="5-1" id="5-1" class="carta"></td>
+            </tr>
+            <tr>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="0-2" id="0-2" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="1-2" id="1-2" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="2-2" id="2-2" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="3-2" id="3-2" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="4-2" id="4-2" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="5-2" id="5-2" class="carta"></td>
+            </tr>
+            <tr>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="0-3" id="0-3" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="1-3" id="1-3" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="2-3" id="2-3" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="3-3" id="3-3" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="4-3" id="4-3" class="carta"></td>
+                <td><img src="img/card_back.png" alt="Unselected card" data-card-id="5-3" id="5-3" class="carta"></td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
